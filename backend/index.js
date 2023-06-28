f5c0075dcbe58f690c9a9d06c1e373fccb401ab1
LordTejas <tejas5225@gmail.com>
2023-06-28 23:09:46 +0530

    Backend Project INIT

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const config = require("./config/config");
+
+// TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
+mongoose
+    .connect(config.mongoose.url, config.mongoose.options)
+    .then(() => console.log(`Connected to on ${config.mongoose.url}`))
+    .catch((err) => console.log(err));
+
+app.listen(config.port, () => {
+    console.log(`Server listening on port ${config.port}`);
+})
